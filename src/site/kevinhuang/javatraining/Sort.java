b4f40e4b1e23735962e75c5e1da010a4aedd6dfb
Kevin Huang <kevinhpf@outlook.com>
2018-04-19 11:14:51 +0800

    Update Sort.java

--- src/site/kevinhuang/javatraining/Sort.java
+++ src/site/kevinhuang/javatraining/Sort.java
@@ -27,7 +27,7 @@ public class Sort
 	/* 1: sortSeclet*/
 	public static void sortSeclet(int[] arr)
 	{
-		for (int i = 0; i < arr.length-1; i++) 
+		for (int i = 0; i < arr.length; i++) 
 		{
 		    for (int j = i+1; j < arr.length; j++) 
 		    {
